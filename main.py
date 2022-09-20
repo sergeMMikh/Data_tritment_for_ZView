3eef95b178e288be9221a3c0a89b2dad3f281006
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-09-20 11:08:34 +0100

    added suffix choice for files filtration

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ if __name__ == '__main__':
     folder_path = input("Input all folder path:\t")
 
     files_list = [files for root, dirs, files in os.walk(folder_path)][0]
-
     print('All files in your data folder:')
     pprint(files_list)
 
+    suffix = input("""Input a keyword for data file filtration or """
+                   """use 'd' to use default 'Impedarce.txt' or """
+                   """use 'n' to use all files: \t""")
+
+    if suffix == 'd':
+        suffix = "Impedarce.txt"
+    elif suffix == 'n':
+        suffix = ".txt"
+    else:
+        pass
+
     # Find impedance text data."
-    data_lst = list(filter(lambda file_name: "Impedarce.txt" in file_name, files_list))
+    data_lst = list(filter(lambda file_name: suffix in file_name, files_list))
 
     print('\nImpedance data files:')
     pprint(data_lst)
